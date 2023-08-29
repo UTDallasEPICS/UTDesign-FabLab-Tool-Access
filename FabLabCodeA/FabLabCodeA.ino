e55010260addd032cdda5be182e600519e2e4842
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2023-08-29 13:32:31 -0500

    Starting Code
    I have made this Arduino coding file to  layout an outline of what we might want the project to do.

---
+++ FabLabCodeA/FabLabCodeA.ino
@@ -0,0 +1,31 @@
+//Code by Ammar Mohammed on 8/29/2023
+/*
+ * PLAN:
+ * 1. Read username from Comet Card
+ * 2. If Comet Card is not already in system add it in
+ *      Some how we need a way to denote whether user has taken 
+ *      training or not.
+ * 3. Else retrieve the username from the Comet Card
+ * 4. Check username attached to boolean variable if they have taken training
+ * 5. If they took the training, then turn machine on
+ * 
+ */
+
+
+bool isSafe = false; //has student taken equipment training course
+bool machineOn = false; //bool used to power machine
+
+String username; //student username from comet card
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  
+}
