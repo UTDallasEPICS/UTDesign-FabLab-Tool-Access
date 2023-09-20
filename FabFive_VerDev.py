d975be733bd3673f2d95ac2bb94f27ca0512c0bf
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2023-09-20 14:03:10 -0500

    Update FabFive_VerDev.py
    Added CardUser class to store both card number and admin status.
    Further updated the add_user() method.

--- FabFive_VerDev.py
+++ FabFive_VerDev.py
@@ -1,4 +1,4 @@
-# FabFive Software, Version: Developement Code
+# FabFive Software, Version: Development Code (DevC 0.2)
 # The development version uses print statements and omits the LCD functionality for testing purposes
 # Author: Ammar Mohammed
 import time
@@ -7,10 +7,16 @@ import time
 # lcd = CharLCD(cols = 16, rows=2, pin_rs=37, pin_e=35, pins_data=(33,31, ))
 
 # Global variables
-card_number = ""
 loop = True
-login = False
 user_list = []
+
+
+class CardUser:
+    # Number stores card number of user
+    # Is_admin (boolean) stores whether user is admin
+    def __init__(self, number, is_admin=False):
+        self.number = number
+        self.is_admin = is_admin
 
 
 # This function will display "start-up" screen for our device
@@ -27,13 +33,6 @@ def boot_up():
     time.sleep(1)
 
 
-# Function - add_user: opens a new file to add the card number
-def add_user(new_user):
-    user_file = open("fabList.txt", "a")
-    user_file.write(new_user + '\n')
-    user_file.close()
-
-
 # Function - dectect_user: reads the card numbers in the file and adds it to user_list
 def detect_user():
     user_file = open("fabList.txt", "r")
@@ -47,38 +46,60 @@ def detect_user():
     print(user_list)
 
 
+# Function - add_user: opens a new file to add the card number
+def add_user(new_user):
+    button_a = False  # yes button
+    button_b = False  # no button
+    print('Call Admin!')  # Perhaps we can have machine beep, or send ping to website
+    # If admin at station, request admin to scan admin card
+    print('Add user to system?')
+
+    if button_a:
+        button_a = False
+        print('Access granted!')
+        user_file = open("fabList.txt", "a")
+        user_file.write(new_user + ' ')
+
+        print('Is user admin?')
+        # Read button inputs again
+        if button_a:
+            user_file.write('ADMIN' + '\n')
+        elif button_b:
+            user_file.write('DEFAULT' + '\n')
+
+        user_file.close()
+
+    elif button_b:
+        button_b = False
+        print('Access denied!')
+
+
 # This is the main loop that will run at launch
 while loop:
-    boot_up()
+    boot_up()  # display welcome message
 
-    detect_user()
+    detect_user()  # load in current user list
 
-    card_number = input()
+    card_number = input()  # retrieve card number
+    current_user = CardUser(card_number)  # add card number to class
 
     # lcd.cursor_pos = (0,0)
-    if card_number == "stop":
+    if card_number == "stop":  # developer bypass functionality
         loop = False
         # lcd.clear()
         # lcd.write_string(u"SYSTEM OFF...")
         print('SYSTEM OFF...')  # Dev code
-    elif card_number in user_list:
+    elif current_user.number in user_list:  # if user is found in list, grant access
         loop = False
-        print('Welcome back ' + card_number)
-    else:
+        print('Welcome back ' + current_user.number)
+    else:  # if user is not in list, goto add_user method
         loop = False
         # lcd.clear()
 
         # lcd.write_string(u"Welcome ")
-        # lcd.write_string(card_number)
-        print('Welcome ' + card_number)  # Dev code
-        add_user(card_number)
+        # lcd.write_string(current_user.number)
+        print('Welcome ' + current_user.number)  # Dev code
+        add_user(current_user.number)
 
         time.sleep(5)
         # lcd.clear()
-
-    '''
-    elif card_number == "":
-        lcd.clear()
-        lcd.write_string(u"BLANK ERROR...")
-        time.sleep(3)
-    '''
