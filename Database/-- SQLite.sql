9d32586be2bdbbe5ed11e6d0c114076b2caa3b89
shruthigna004 <125151261+shruthigna004@users.noreply.github.com>
2023-11-25 20:17:09 -0600

    Update -- SQLite.sql

--- Database/-- SQLite.sql
+++ Database/-- SQLite.sql
@@ -1,18 +1,13 @@
 -- SQLite
---CREATE TABLE users1 (userID TEXT NOT NULL PRIMARY KEY,
---AdminStatus NOT NULL);
 
--- CREATE TABLE logTable (userID TEXT NOT NULL PRIMARY KEY,
--- MachineType NOT NULL, TimeUsed NOT NULL, TimeStamp);
-
---  INSERT INTO users1 (userID, AdminStatus)
---  VALUES ('BSX210412', '1');
-
--- INSERT INTO logTable (userID, MachineType, TimeUsed, TimeStamp)
--- VALUES ('AXS210411', 'FlameThrower', '300', '2021-04-12 21:00:00');
-
-SELECT * FROM users1;
---SELECT * FROM users1 WHERE userID = 'BSX210412';
---SELECT  * FROM logTable;
+CREATE DATABASE  IF NOT EXISTS Fablab;
+USE Fablab;
+CREATE TABLE USERS (
+userID INTEGER NOT NULL PRIMARY KEY,
+AdminStatus INTEGER NOT NULL, 
+MachineType TEXT NOT NULL, 
+Date TEXT NOT NULL, 
+StartTime TEXT NOT NULL,
+EndTime TEXT NOT NULL);
 
 
