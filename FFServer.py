f09113201abd985390fac34512e46bb7f095fa6a
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2023-12-08 18:03:42 -0600

    Added FFServer.py
    Here is the Python Server code that works by receiving a string that is then used for the MYSQL connector EXECUTE command.

---
+++ FFServer.py
@@ -0,0 +1,35 @@
+import socket
+import mysql.connector
+bufferSize = 1024
+ServerPort = 2222
+ServerIP = '192.168.254.38'
+
+msgFromServer = "Thanks! Server received timelog data."
+bytesToSend = msgFromServer.encode('utf-8')
+
+RPIsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+RPIsocket.bind((ServerIP, ServerPort))
+
+print('Server is up and listening...')
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="fablab"
+)
+my_cursor = db.cursor()
+
+#'''
+while True:
+    logdata, address = RPIsocket.recvfrom(bufferSize)
+    logdata = logdata.decode('utf-8')
+    print(logdata)
+    print('Client Address: ', address[0])
+    RPIsocket.sendto(bytesToSend, address)
+    mysql_command = f'INSERT INTO timelog(userID, adminStatus, machineType, date, startTime, endTime) VALUES({logdata})'
+    my_cursor.execute(mysql_command)
+    db.commit()
+    if logdata == 'shutdown':
+        break
+#'''
