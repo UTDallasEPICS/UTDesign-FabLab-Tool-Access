ff88a3d6948164fa76c1bf9a345bdb8463cdf388
AaduMaadu <aadeshsenthil@gmail.com>
2023-12-15 17:50:05 -0600

    Update script

--- Raspberry_Pi/startup_script.sh
+++ Raspberry_Pi/startup_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # wait for desktop to load
-sleep 15 
+sleep 20 
 
 # Change path accordingly to script location
 cd /home/pi/Desktop/UTDesign-FabLab-Tool-Access/Raspberry_Pi
-lxterminal -e python3 FabFivePi.py > /home/pi/Desktop/UTDesign-FabLab-Tool-Access/Raspberry_Pi/script_log.txt 2>&1
+xterm -e python3 FabFivePi.py > /home/pi/Desktop/UTDesign-FabLab-Tool-Access/Raspberry_Pi/script_log.txt 2>&1
